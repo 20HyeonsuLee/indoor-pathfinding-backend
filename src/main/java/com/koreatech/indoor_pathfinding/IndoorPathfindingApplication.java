e17bb0ea8b7a490b86763a045048112cbbb32e03
leehyeonsu <leehyeonsu4888@gmail.com>
2026-01-29 00:51:53 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/koreatech/indoor_pathfinding/IndoorPathfindingApplication.java
@@ -0,0 +1,13 @@
+package com.koreatech.indoor_pathfinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorPathfindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorPathfindingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreatech/indoor_pathfinding/IndoorPathfindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreatech.indoor_pathfinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorPathfindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
